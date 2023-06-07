6c8a9e8a573131b9f1f6d3331664d42546a4df81
Javier Almarza <javieralmarzabucarey@gmail.com>
2023-06-07 00:07:31 -0400

    moviendo las weas a src

--- src/characters/jermaine/jermaine.gd
+++ src/characters/jermaine/jermaine.gd
@@ -1,10 +1,9 @@
 # Dependencias.
-extends Area2D
+extends CharacterBody2D
 signal hit
 
 # Variables.
 @export var speed : int = 100;
-var velocity := Vector2.ZERO;
 var axis: Vector2;
 
 # Metodos.
@@ -19,24 +18,26 @@ func motion_ctrl() -> void:
 	velocity.x = (get_axis().x * speed);
 	velocity.y = (get_axis().y * -speed);
 
-func move_and_slide(delta) -> void:
-	position = position + (velocity * delta)
-
-	if (velocity.x != 0):
-		$Sprite.play("Walk")
-		$Sprite.flip_h = (velocity.x < 0);
+func move_animation() -> void:
+	if (velocity.x != 0 and velocity.y == 0):
+		$Sprite.play("Run")
+		$Sprite.flip_h = (velocity.x < 0)
+	elif (velocity.y != 0):
+		$Sprite.play("Double jump");
+		$Sprite.flip_h = (velocity.x < 0)
 	else:
 		$Sprite.play("Idle")
 
 # Procesos.
-func _process(delta):
+func _physics_process(_delta):
+	velocity.y += 50
+	move_and_slide();
+	
+func _process(_delta):
 	# Animaciones
 	$Sprite.play()
 	
 	# Control del jugador.
 	motion_ctrl();
-	move_and_slide(delta)
-	
-	
-	
+	move_animation();
 	
