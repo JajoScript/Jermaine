b28f707704bf7310a58c8cf60091eaabc4ece3fe
Javier Almarza <javieralmarzabucarey@gmail.com>
2023-06-21 15:08:21 -0400

    Menu principal y splash screen

---
+++ src/screens/MainScreen/main.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+
+func _on_btn_nueva_partida_button_up():
+	print("Nueva partida!")
+
+
+func _on_btn_cargar_partida_button_up():
+	print("Cargar partida!")
+	pass # Replace with function body.
+
+
+func _on_btn_opciones_button_up():
+	print("Opciones!")
+	pass # Replace with function body.
+
+
+func _on_btn_salir_button_up():
+	print("Salir!")
+	pass # Replace with function body.
