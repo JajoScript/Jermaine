cd5862d0d77d46d0a6e8b078d8b74dba689fd1d2
Javier Almarza <javieralmarzabucarey@gmail.com>
2023-06-27 01:42:33 -0400

    implementación de vista de muerte

--- src/screens/NewGameScreen/new_game_scene.gd
+++ src/screens/NewGameScreen/new_game_scene.gd
@@ -9,4 +9,4 @@ func _process(_delta):
 	pass
 
 func _on_timer_timeout():
-	get_tree().change_scene_to_file("res://src/levels/Mundo 01/Mundo_01.tscn")
+	get_tree().change_scene_to_file("res://src/levels/Mundo_01/Mundo_01.tscn")
